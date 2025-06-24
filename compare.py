d8b7ab5fcc674070cfa87df0acb7cfb809bfd3bd
OM_JAISWAL <omjaiswal1802@gmail.com>
2025-06-24 20:28:30 +0530

    Create compare.py

---
+++ compare.py
@@ -0,0 +1,9 @@
+x=int(input("ENTER NUMBER: "))
+y=int(input("ENTER NUMBER: "))
+
+if x<y:
+    print(f"{x} is lesser than {y}")
+elif y<x:
+    print(f"{y} is lesser than {x}") 
+else:
+    print(f"{x} is equal to {y}")

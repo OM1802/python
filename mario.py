6d55d26c126827adfe485ceba3f7797018d95c96
OM_JAISWAL <omjaiswal1802@gmail.com>
2025-06-27 12:27:31 +0530

    update

--- mario.py
+++ mario.py
@@ -1,6 +1,13 @@
 def main():
     print_column(3)
     print_row(4)
+    print_square(3)
+    
+def print_square(dim):
+    for i in range(dim):
+        for j in range(dim):
+            print("-", end="")
+        print()
     
 def print_column(h):
     print("# \n" * h, end="")

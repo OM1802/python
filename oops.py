41e924166d8c9a4a649735ddec8d4d18fa365371
OM_JAISWAL <omjaiswal1802@gmail.com>
2025-08-24 02:30:34 +0530

    update

--- oops.py
+++ oops.py
@@ -1,6 +1,19 @@
 class Student:
-    def __init__(self,fullname):#constructor
-        self.name=fullname#Adding new student in database..
+    def __init__(self,name,marks):#constructor
+        self.name=name
+        self.marks=marks
+    
+    @staticmethod #decorator
+    def hello():
+        print("HELLO!! ")
         
-s1=Student("Max")
-print(s1.name)
+    def get_avg(self):
+        sum=0
+        for val in self.marks:
+            sum+=val
+        print(sum/3)
+        
+
+s1=Student("BEN", [85,25,46])
+s1.hello()
+s1.get_avg()
